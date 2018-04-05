8c151a5375c65903581094f51a4bc72118a76d91
ekaputril <ekaapue@gmail.com>
2018-04-05 19:39:43 +0700

    first commit

---
+++ hello-bootstrap/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+<title>Hello Bootstrap</title>
+</head>
+
+
+	<h1 class="text-center">Hello Bootstrap</h1>
+	
+
+<body>
+
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
